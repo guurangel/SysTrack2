77596e2b23d87a83374ede07b413f511e84a398f
Gustavo <gustavo_dgk@outlook.com>
2025-09-24 01:12:42 -0300

    update essencial configs

---
+++ src/main/java/com/example/systrack2/SysTrack2Application.java
@@ -0,0 +1,13 @@
+package com.example.systrack2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysTrack2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysTrack2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/systrack2/SysTrack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systrack2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysTrack2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
